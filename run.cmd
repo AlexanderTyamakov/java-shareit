d5a74dfe51b56d2ebb45c132f39ee54c166e00f1
Alexander Tyamakov <tiamakoff@yandex.ru>
2023-04-07 02:58:39 +0300

    fix sprint 16

--- run.cmd
+++ run.cmd
@@ -1,4 +1,2 @@
 docker-compose up -d
 
-docker exec -it shareit-server /bin/bash -c "ln -snf /usr/share/zoneinfo/Europe/Moscow /etc/localtime && dpkg-reconfigure -f noninteractive tzdata"
-docker exec -it shareit-gateway /bin/bash -c "ln -snf /usr/share/zoneinfo/Europe/Moscow /etc/localtime && dpkg-reconfigure -f noninteractive tzdata"
